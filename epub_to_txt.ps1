028d761e7909d407fd94bf038101e9731e072529
MindOfMatter <martineaum22@outlook.com>
2024-02-04 02:39:13 -0500

    fix by move $outputTxtFolderPath

--- epub_to_txt.ps1
+++ epub_to_txt.ps1
@@ -3,15 +3,14 @@ Clear-Host
 
 # Load setup.json content
 $setupJsonPath = Join-Path -Path $Global:currentPath -ChildPath "setup.json"
-$setupConfig = Get-Content -Path $setupJsonPath -Raw | ConvertFrom-Json
+$setupConfig = Get-Content -Path $setupJsonPath -Raw -Encoding UTF8 | ConvertFrom-Json
 
 $Global:currentPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 function ConvertEpubToTxt {
-    [CmdletBinding()]
-    $ebookName = [System.IO.Path]::GetFileNameWithoutExtension($Global:ebookPath)
-    $outputTxtFolderPath = Join-Path -Path $Global:currentPath -ChildPath "extracted_chapters\$ebookName"
+
     & "python" "$Global:currentPath\epub_to_txt_by_chapter.py" $Global:ebookPath
+
     Write-Host "`nEpub to txt conversion done. Path: $outputTxtFolderPath"
 }
 
@@ -21,7 +20,11 @@ function ConvertEpubToTxtAndEdit {
     $skipConversion = Read-Host "Skip the Epub to txt conversion? (Y/N, default: N)"
     if ($skipConversion -ne 'Y' -and $skipConversion -ne 'y') {
         ConvertEpubToTxt
+
         Start-Sleep -Seconds 2
+
+        $ebookName = [System.IO.Path]::GetFileNameWithoutExtension($Global:ebookPath)
+        $outputTxtFolderPath = Join-Path -Path $Global:currentPath -ChildPath "extracted_chapters\$ebookName"
 
         Invoke-Item $outputTxtFolderPath
     } else {
