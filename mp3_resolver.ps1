ac25e8f3484197b35a18b3e60bcc1eb921e99cd0
MindOfMatter <martineaum22@outlook.com>
2024-02-04 08:46:47 -0500

    add script to fix remaining mp3 tasks

---
+++ mp3_resolver.ps1
@@ -0,0 +1,43 @@
+﻿# Clear the screen
+Clear-Host
+
+# Load setup.json content
+$setupJsonPath = Join-Path -Path $Global:currentPath -ChildPath "setup.json"
+$setupConfig = Get-Content -Path $setupJsonPath -Raw -Encoding UTF8 | ConvertFrom-Json
+
+$Global:currentPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+
+# Accessing the values
+$Global:defaultLibraryPath = $setupConfig.default_library_path
+$Global:ebookPath = $setupConfig.default_ebook_path
+
+$EpubToTxtScriptPath = "$Global:currentPath\epub_to_txt.ps1"
+. $EpubToTxtScriptPath
+
+$TxtToMp3ScriptPath = "$Global:currentPath\txt_to_mp3.ps1"
+. $TxtToMp3ScriptPath
+
+function GetEbookPath {
+    [CmdletBinding()]
+    $dialog = New-Object System.Windows.Forms.OpenFileDialog
+    $dialog.InitialDirectory = $Global:defaultLibraryPath
+    $dialog.Filter = "ePub files (*.epub)|*.epub"
+    $result = $dialog.ShowDialog()
+    if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+        return $dialog.FileName
+    }
+    Write-Warning "No eBook selected. Exiting script."
+    exit
+}
+
+# Main Script Execution
+Initialize
+
+# Setup and User Prompts
+if (-not $Global:ebookPath) {
+    $Global:ebookPath = GetEbookPath
+}
+
+ConvertAllWAVToMP3 -InputFolderPath $outputFolderPath
+
+CreateOrUpdatePlaylist -OutputFolderPath $outputFolderPath
